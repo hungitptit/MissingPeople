eebfebb2358de32dacf7574cff7c202726e3be70
hungitptit <61471882+hungitptit@users.noreply.github.com>
2021-07-18 14:32:42 +0700

    update

--- center/admin.py
+++ center/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import *
 admin.site.register(MissingPeople)
+admin.site.register(UserProfile)
+admin.site.register(ReportedPeople)
+admin.site.register(MatchingPairs)
 # Register your models here.
