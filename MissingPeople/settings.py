6fe0978ac8884254ae4fa07f035d2334ece4e382
hungitptit <61471882+hungitptit@users.noreply.github.com>
2021-07-18 21:36:16 +0700

    update

--- MissingPeople/settings.py
+++ MissingPeople/settings.py
@@ -114,7 +114,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 ]
-SITE_ID = 2
+SITE_ID = 4
 
 LOGIN_REDIRECT_URL = '/missing'
 LOGOUT_REDIRECT_URL = '/missing'
